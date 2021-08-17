e515d1784013b0cbc4a37209639c481c254f5a5f
Nathan Macfarlane <nathanmacfarlane@Nathans-MacBook-Pro.local>
2021-08-17 16:09:19 -0700

    remove comments

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,40 +1,4 @@
-const path = require(`path`);
 const { createFilePath } = require(`gatsby-source-filesystem`);
-
-// exports.createPages = async ({ graphql, actions, reporter }) => {
-//   const { createPage } = actions;
-
-//   const result = await graphql(`
-//     query {
-//       projects: allMarkdownRemark {
-//         edges {
-//           node {
-//             id
-//             frontmatter {
-//               title
-//               id
-//               slug
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `);
-
-//   if (result.errors) {
-//     reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query');
-//   }
-
-//   const projects = result.data.projects.edges;
-
-//   projects.forEach(({ node }, index) => {
-//     createPage({
-//       path: `/project${node.frontmatter.slug}`,
-//       component: path.resolve(`./src/pages/project.js`),
-//       context: { id: node.id },
-//     });
-//   });
-// };
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
